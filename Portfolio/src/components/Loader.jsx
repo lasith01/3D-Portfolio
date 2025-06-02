a5c1220916312ad1d9d66189eb67e10c7f8e80bc
Lasith Amanda <lasithamanda1999@example.com>
2025-06-02 19:50:48 +0530

    Add astronaut floating model in hero section

---
+++ Portfolio/src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import { Html, useProgress } from '@react-three/drei'
+import React from 'react';
+
+const Loader = () => {
+  const {progress} = useProgress();
+  return <Html center className='text-xl font-normal text-center'>{progress}% loaded</Html>;
+};
+
+export default Loader;
